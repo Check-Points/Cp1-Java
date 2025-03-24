09a13fb68ab9a1f88e90275faeba440eb5c29621
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-24 11:43:05 -0300

    update

---
+++ src/br/com/fiap/bean/DespesaFamiliar.java
@@ -0,0 +1,23 @@
+//RM: 554682 - Milena Codinhoto
+//RM: 557929 - Evellyn Choque
+//RM: 86833 - Carolina Santana
+
+package br.com.fiap.bean;
+
+public class DespesaFamiliar {
+    public double rendaFamiliar, gastoComLuz, gastoComAgua, gastoComInternet, valorMensalidadeDaAcademia;
+    public int numeroDeMoradores;
+
+    public double calcularTotalDeDespesas() {
+
+
+        return
+    }
+
+    public double calcularRendaFamiliar() {
+
+
+        return
+    }
+
+}
